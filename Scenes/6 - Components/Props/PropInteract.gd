619b98694149ff43fe9ce59288a772e1fd5dc27b
WilliamG99 <truhappygameing@gmail.com>
2024-08-02 11:27:13 +0100

    Update - Throwing forward working well, small bug that sometimes throws prop downwards. Will maybe try a lock-on system.

--- Scenes/6 - Components/Props/PropInteract.gd
+++ Scenes/6 - Components/Props/PropInteract.gd
@@ -6,3 +6,5 @@ var is_holding : bool
 var prop_rotation_degrees_y : int
 var prop_position : Vector3
 var prop_aim_direction : Vector3
+var prop_aim_direction_y : Vector3
+var prop_throw_speed : int
