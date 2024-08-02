4665d901df2093e4c1c760a0b380ee31f3d5bb5a
dselimovicc <dselimovic02@gmail.com>
2024-08-02 16:25:56 +0200

    Update - add SFX to menu buttons

--- Scenes/6 - Menu/Scripts/game_menu.gd
+++ Scenes/6 - Menu/Scripts/game_menu.gd
@@ -1,22 +1,67 @@
 extends Control
 
 # Signal that indicates that Resume button was pressed
-signal resume()
+signal resume
+
+# Get button SFX references
+@onready var click = $Audio/Click
+@onready var select = $Audio/Select
+
+# When click sound finishes, look according to bools what action to do
+var send_resume_signal = false
+var return_to_title = false
+var exit = false
 
 func _on_button_exit_pressed():
-	# Close the game
-	print("game_menu.gd::8::EXIT_BUTTON_PRESSED")
-	print("Closing the game.")
-	get_tree().quit()
+	# Play SFX
+	click.play()
+	# Set bool
+	exit = true
 
 func _on_button_resume_pressed():
-	# Emit signal to main.gd
-	print("game_menu.gd::14::RESUME_BUTTON_PRESSED")
-	print("Closing te game menu.")
-	resume.emit()
+	# Play SFX
+	click.play()
+	# Set bool
+	send_resume_signal = true
+
 
 func _on_button_return_pressed():
-	print("game_menu.gd::20::RETURN_BUTTON_PRESSED")
-	print("Returning to title screen.")
-	# Return to title scene
-	get_tree().change_scene_to_file("res://Scenes/6 - Menu/Scenes/start_menu.tscn")
+	# Play SFX
+	click.play()
+	# Set bool 
+	return_to_title = true
+
+func _on_click_finished():
+	if send_resume_signal:
+		# Reset bool
+		send_resume_signal = false
+		# Emit signal to main.gd
+		print("game_menu.gd::14::RESUME_BUTTON_PRESSED")
+		print("Closing te game menu.")
+		resume.emit()
+		
+	if return_to_title:
+		# Reset bool
+		return_to_title = false
+		# Return to title scene
+		print("game_menu.gd::38::RETURN_BUTTON_PRESSED")
+		print("Returning to title screen.")
+		get_tree().change_scene_to_file("res://Scenes/6 - Menu/Scenes/start_menu.tscn")
+		
+	if exit:
+		# Reset bool 
+		exit = false
+		# Close the game
+		print("game_menu.gd::8::EXIT_BUTTON_PRESSED")
+		print("Closing the game.")
+		get_tree().quit()
+		
+
+func _on_button_resume_mouse_entered():
+	select.play()
+
+func _on_button_return_mouse_entered():
+	select.play()
+
+func _on_button_exit_mouse_entered():
+	select.play()
--- Scenes/6 - Menu/Scripts/start_menu.gd
+++ Scenes/6 - Menu/Scripts/start_menu.gd
@@ -2,16 +2,54 @@ extends Control
 
 # Get music reference
 @onready var music = $Music
+# Get SFX References
+@onready var click = $Audio/Click
+@onready var select = $Audio/Select
+
+var start_game = false
+var quit_game = false
 
 func _on_button_start_pressed():
-	# Start the game
-	get_tree().change_scene_to_file("res://Scenes/main.tscn")
+	# Play SFX
+	click.play()
+	# Update bool
+	start_game = true
 
 func _on_button_quit_pressed():
-	# Close the window
-	get_tree().quit()
+	# Play SFX
+	click.play()
+	# Update bool
+	quit_game = true
 
+
+func _on_click_finished():
+	if start_game:
+		# Reset bool
+		start_game = false
+		# Start the game
+		print("start_menu.gd::10::START_BUTTON_PRESSED")
+		print("Starting the game.")
+		get_tree().change_scene_to_file("res://Scenes/main.tscn")		
+	if quit_game:
+		# Close the window
+		print("start_menu.gd::35::QUIT_BUTTON_PRESSED")
+		print("Closing the game.")
+		get_tree().quit()
+		
+
+# ************************
+# HANDLE SOUNDS
+# ************************
 
 # Loops the music
 func _on_music_finished():
+	# Restart music
 	music.play()
+
+func _on_button_start_mouse_entered():
+	# Play SFX
+	select.play()
+
+func _on_button_quit_mouse_entered():
+	# Play SFX
+	select.play()
