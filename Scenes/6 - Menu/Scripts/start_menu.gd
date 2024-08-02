333e779e30e2d4677cca192179e3758caadf1756
WilliamG99 <truhappygameing@gmail.com>
2024-08-02 12:27:55 +0100

    Update - Fixed missing dependencies

--- Scenes/6 - Menu/Scripts/start_menu.gd
+++ Scenes/6 - Menu/Scripts/start_menu.gd
@@ -5,7 +5,7 @@ extends Control
 
 func _on_button_start_pressed():
 	# Start the game
-	get_tree().change_scene_to_file("res://Scenes/level.tscn")
+	get_tree().change_scene_to_file("res://Scenes/main.tscn")
 
 func _on_button_quit_pressed():
 	# Close the window
